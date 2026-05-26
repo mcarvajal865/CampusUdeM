bb185aa432de78404bdd33b7e453497608e62979
Manuela0613 <martinezgilmanuela@gmail.com>
2026-05-25 21:04:38 -0500

    Datos

--- grafo/datos_campus.py
+++ grafo/datos_campus.py
@@ -13,6 +13,14 @@ def crearCampus():
     )
 
     campus.agregarConexion(
+        'Entrada Principal',
+        'Parqueadero',
+        120, 2, 5,
+        True,
+        'disponible'
+    )
+
+    campus.agregarConexion(
         'Bloque 1',
         'Bloque 2',
         60, 1, 3,
@@ -20,6 +28,46 @@ def crearCampus():
         'disponible'
     )
 
-    # TODAS las demás conexiones...
+    campus.agregarConexion(
+        'Bloque 1',
+        'Biblioteca',
+        100, 2, 2,
+        True,
+        'bloqueado'
+    )
+
+    campus.agregarConexion(
+        'Bloque 2',
+        'Bloque 3',
+        70, 1, 2,
+        True,
+        'disponible'
+    )
+
+    campus.agregarConexion(
+        'Bloque 2',
+        'Cafeteria',
+        90, 2, 5,
+        True,
+        'disponible'
+    )
+
+    campus.agregarConexion(
+        'Bloque 3',
+        'Laboratorio',
+        50, 1, 1,
+        False,
+        'disponible'
+    )
+
+    campus.agregarConexion(
+        'Bloque 3',
+        'Bloque 4',
+        80, 2, 3,
+        True,
+        'disponible'
+    )
+
+
 
     return campus
